68a7f0e93dded6ca232dd66b2108e837dca44d95
nsykim <noahsykim@gmail.com>
2024-02-27 16:27:37 -0500

    added colors to icon text and differentiation between selected & non-selected

--- app/lib/pages/chat.dart
+++ app/lib/pages/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ChatPage extends StatelessWidget {
@@ -8,33 +9,44 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xff202020),
-          appBar: AppBar(
+        backgroundColor: const Color(0xff202020),
+        appBar: AppBar(
             centerTitle: true,
-            backgroundColor: const Color(0xff202020),
-              title: Row (
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+            backgroundColor: Color(0xff202020),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Image.asset(
-              'assets/logo_trans.png',
-              height: 30,
-            ),
-              Text(
-                '  boileraide',
-                style: GoogleFonts.poppins(
-                  color: const Color(0xffD0ad50),
-                  fontWeight: FontWeight.bold,
+                  'assets/logo_trans.png',
+                  height: 30,
                 ),
-              )
-            ],)
-            
-          ),
-          body: Container(
-            // margin: const EdgeInsets.all(50),
-            // padding: const EdgeInsets.all(50),
-            color: const Color.fromARGB(68, 134, 148, 155),
-            child: const Text('placeholder'),
-          )),
+                Text(
+                  '  boileraide',
+                  style: GoogleFonts.poppins(
+                    color: const Color(0xffD0ad50),
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            )),
+        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: const Color(0xff202020),
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble),
+                label: 'Chat',
+                // backgroundColor:Color(0xffD0ad50),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark),
+                label: 'Saved',
+              ),
+            ],
+             selectedItemColor: const Color(0xffD0ad50),
+             unselectedItemColor: const Color.fromARGB(255, 113, 83, 5),
+             ),
+      ),
     );
   }
 }
