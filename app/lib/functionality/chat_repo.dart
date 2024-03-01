054b24377943b866feea67ef99dacf7c370da9cf
nsykim <noahsykim@gmail.com>
2024-02-29 19:06:25 -0500

    chat_repo should be functional... now time to implement the actual chat feature

--- app/lib/functionality/chat_repo.dart
+++ app/lib/functionality/chat_repo.dart
@@ -1,15 +1,13 @@
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart'; //used store chat data
 import 'dart:async';
-import 'package:get_it/get_it.dart';
-import 'package:path_provider/path_provider.dart';
 import 'messages.dart';
 
 class ChatRepo {
   //late means it will be initalized before it's used.
   late final Database db;
   //list of chatIDs so we know if this is a new chat and need to open a new store
-  final List<int> chatIDs = [];
+  final Set<int> chatIDs = {};
 
   ChatRepo() {
     //constructor... will create a new database called chat_database
@@ -21,18 +19,24 @@ class ChatRepo {
     db = await databaseFactoryIo.openDatabase('chat_database.db');
   }
 
-  Future<void> createNewStore(Message message) async {
+  Future<bool> createNewStore(Message message) async {
     if (!chatIDs.contains(message.chatID)) {
       chatIDs.add(message.chatID);
       StoreRef<int, Map<String, dynamic>> store =
           intMapStoreFactory.store('chat_$message.chatID');
-      await store
-          .record(0)
-          .put(db, {'dummy': 'data'}); //just put dummy data in form now
+      await store.add(db,message.toJson()); //store Json of message in store which is in database db
+      return true; //new store is created, first message stored
+    } else {
+      return false; //store already existed, need to store message
     }
   }
 
   Future<void> storeMessage(Message message) async {
-    createNewStore(message); //make sure there is a store to hold messages
+    if (!(await createNewStore(message))) {
+      //if false, need to store the message still
+      StoreRef<int, Map<String, dynamic>> store =
+          intMapStoreFactory.store('chat_${message.chatID}');
+      await store.add(db, message.toJson());
+    }
   }
 }
