86d8d2e7b3ae9f84d381b0e63a6af4507414550c
nsykim <noahsykim@gmail.com>
2024-03-18 16:57:25 -0400

    new_chat_page initial implementation finished

--- app/lib/pages/new_chat_page.dart
+++ app/lib/pages/new_chat_page.dart
@@ -29,43 +29,84 @@ class _ChatPageState extends State<ChatPage> {
 
   Future<void> _generateNewID() async {
     if (_chatID == 0) {
-      int maxID = await _chatRepo.getMaxID(); //get max ID if chatID is 0 (assumed uninitialized)
-      if (maxID  != 0) { //returns 0 if search returns NULL (first)
-          setState(() {
-            _chatID = maxID + 1;
-          });
-        }
+      int maxID = await _chatRepo
+          .getMaxID(); //get max ID if chatID is 0 (assumed uninitialized)
+      if (maxID != 0) {
+        //returns 0 if search returns NULL (first)
+        setState(() {
+          _chatID = maxID + 1;
+        });
       }
     }
+  }
+
+  Future<void> _sendMessage() async {
+    String content = _messageController.text.trim(); //message content
+    //.trim removes leading and trailing whitespaces
+    if (content.isNotEmpty) {
+      //if contains info
+      Message sent = Message(
+          //fill out message class
+          chatID: _chatID,
+          content: content,
+          timestamp: DateTime.now(),
+          isUser: true);
+      await _chatRepo.storeMessage(sent);
+      //clear the chatbar
+      _messageController.clear();
+      setState(() {});
+      //ui update
+    }
+  }
 
   Widget build(BuildContext context) {
     return ChatsPageLayout(
         body: Column(children: [
       Expanded(
-          child: FutureBuilder<List<Message>>(
-              future: _chatRepo.getAllMessages(_chatID),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  //error message
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  final messages = snapshot.data ?? [];
-                  return ListView.builder(
-                    itemCount: messages.length,
-                    itemBuilder : (context, index) {
-                      final message = messages[index];
-                      return ListTile(
-                        title: Text(message.content),
-                        subtitle: Text(message.timestamp.toString()),
-                        );
-                    },
-                    );
-                }
-              },
+        child: FutureBuilder<List<Message>>(
+          future: _chatRepo.getAllMessages(_chatID),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              //error message
+              return Text('Error: ${snapshot.error}');
+            } else {
+              final messages = snapshot.data ?? [];
+              return ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  final message = messages[index];
+                  return ListTile(
+                    title: Text(message.content),
+                    subtitle: Text(message.timestamp.toString()),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row (
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _messageController,
+                decoration: const InputDecoration(
+                  hintText:  'What sounds good to eat?',
+                ),
               ),
-              ),
+            ),
+            IconButton(
+              onPressed: _sendMessage,
+              icon: const Icon(Icons.send),
+            )
+          ]
+        )
+      ),
+
     ]));
   }
 }
