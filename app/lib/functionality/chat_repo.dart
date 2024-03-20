6f98f85ad7e09548fa4f75f04bc7032353c3ef3c
nsykim <noahsykim@gmail.com>
2024-03-20 19:13:41 -0400

    cleaned up and streamlined chat_repo

--- app/lib/functionality/chat_repo.dart
+++ app/lib/functionality/chat_repo.dart
@@ -1,36 +1,52 @@
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart'; //used store chat data
-// import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'messages.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChatRepo {
   //late means it will be initalized before it's used.
   late final Database db;
-  //list of chatIDs so we know if this is a new chat and need to open a new store
-  final Set<int> chatIDs = {};
   bool _initalized = false;
   ChatRepo._(); //private constructor
   static final ChatRepo _instance = ChatRepo._();
-  Logger logger = Logger();
-
   factory ChatRepo() => _instance; //just makes sure there is only ever one
+  Logger logger = Logger();
 
   Future<void> initializeDatabase() async {
     //opens or creates if it doesn't already exist
     //gets dynamic path to app storage... won't be deleted when app is deleted
-    final appDocumentDir = await getApplicationDocumentsDirectory();
-    final dbPath = '${appDocumentDir.path}/chat_database.db';
-    db = await databaseFactoryIo.openDatabase(dbPath);
-    _initalized = true;
+    if (!_initalized) {
+      final appDocumentDir = await getApplicationDocumentsDirectory();
+      final dbPath = '${appDocumentDir.path}/chat_database.db';
+      db = await databaseFactoryIo.openDatabase(dbPath);
+      _initalized = true;
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setInt('ID', 1); //set ID to 1 at beginning
+      //initialize to 1... save 0 to store chatlog names
+    }
   }
 
-  Future<void> initalizeIfNeeded() async {
-    if (!_initalized) {
-      await initializeDatabase();
-      _initalized = true;
+  Future<int> _getCurrentID() async {
+    final prefs = await SharedPreferences.getInstance();
+    final int? ID = prefs.getInt('ID');
+    if (ID == null) {
+      throw Exception('no ID found!');
+    } else {
+      return ID;
     }
+  }
+
+  Future<int> _updateChatID() async {
+    //update shared preferences to save this after app is closed
+    final prefs = await SharedPreferences.getInstance();
+
+    final currentID = prefs.getInt('ID');
+    final newID = currentID! + 1;
+    await prefs.setInt('ID', newID);
+    return newID;
   }
 
   Future<bool> createNewStore(Message message) async {
@@ -38,7 +54,10 @@ class ChatRepo {
       chatIDs.add(message.chatID);
       StoreRef<int, Map<String, dynamic>> store =
           intMapStoreFactory.store('chat_$message.chatID');
-      await store.add(db, message.toJson());  //store Json of message in store which is in database db
+      await store.add(
+          db,
+          message
+              .toJson()); //store Json of message in store which is in database db
       return true; //new store is created, first message stored
     } else {
       return false; //store already existed, need to store message
@@ -54,15 +73,8 @@ class ChatRepo {
       print('Message stored successfully'); // Log successful message storage
     } catch (e) {
       print('Error storing message: $e'); // Log error storing the message
-      logger.e('Error storing message: $e'); // Log error using logger
-      // rethrow e; // Rethrow the exception to propagate it up the call stack
-    }  
-    // if (!(await createNewStore(message))) {
-    //   //if false, need to store the message still
-    //   StoreRef<int, Map<String, dynamic>> store =
-    //       intMapStoreFactory.store('chat_${message.chatID}');
-    //   await store.add(db, message.toJson());
-    // }
+      logger.e('Error storing message: $e'); // Log error using loggerthe
+    }
   }
 
   Future<List<Message>> getAllMessages(int chatID) async {
@@ -74,23 +86,5 @@ class ChatRepo {
     final messages =
         snapshot.map((record) => Message.fromJson(record.value)).toList();
     return messages;
-  }
-
-  Future<int> getMaxID() async {
-    //go to this store
-    if (db == false) {
-      throw Exception("Database has not been initalized yet");
-    }
-    final store = intMapStoreFactory.store('chat_store');
-    final finder = Finder(sortOrders: [SortOrder('chatID', false)]);
-    final snapshot = await store.findFirst(db, finder: finder);
-    if (snapshot != null) {
-      final chatID = snapshot.value['chatID'] as int?;
-      //? operator used to access something that might be null. returns null if is null or returns object if not
-      if (chatID != null) {
-        return chatID;
-      }
-    }
-    return 0;
   }
 }
